2ef3e4016c90250ead3ed576de3f553db07f9ca9
Poguri Diya <diya>
2025-11-15 16:37:05 +0530

    terraform

---
+++ 00-vpc/main.tf
@@ -0,0 +1,25 @@
+module "vpc" {
+    source = "https://github.com/poguridiya19-gif/terraform-aws-vpc"
+
+    #VPC
+    vpc_cidr = var.vpc_cidr
+    project_name = var.project_name
+    environment = var.environment
+    vpc_tags = var.vpc_tags
+
+    # PUBLIC SUBNETS
+    public_subnet_cidrs = var.public_subnet_cidrs
+
+    # private subnets
+    private_subnet_cidrs = var.private_subnet_cidrs
+
+    # database subnets
+    database_subnet_cidrs = var.database_subnet_cidrs
+
+    is_peering_required = true
+
+}
+
+# data "aws_availabilty_zones" "available" {
+#     state = "available"
+# }
