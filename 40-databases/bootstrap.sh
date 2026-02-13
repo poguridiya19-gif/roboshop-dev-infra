6f8f386589bf7c50a58c3af1232376424ab793f4
Poguri Diya <diya>
2026-02-13 12:38:55 +0530

    terraform

--- 40-databases/bootstrap.sh
+++ 40-databases/bootstrap.sh
@@ -23,4 +23,4 @@ else
     git clone $REPO_URL
     cd $ANSIBLE_DIR
 fi
-ansible-playbook -e component=$component environment=$environment main.yaml
+ansible-playbook -e component=$component -e environment=$environment main.yaml
