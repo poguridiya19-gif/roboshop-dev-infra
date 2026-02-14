0990e69c8695e0c5a10718b5eb95fa298d9f3541
Poguri Diya <diya>
2026-02-14 18:16:05 +0530

    terraform

--- 60-catalogue/main.tf
+++ 60-catalogue/main.tf
@@ -84,7 +84,7 @@ resource "aws_launch_template" "catalogue"{
   vpc_security_group_ids = [local.catalogue_sg_id]
 
   # tags attached to the instance
-  tags_specifications {
+  tag_specifications {
     resource_type = "instance"
 
     tags = merge(
