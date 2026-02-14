3ba92e09f25b3c9cde627f4d83b6982bbe25cac5
Poguri Diya <diya>
2026-02-14 16:40:56 +0530

    terraform

--- 60-catalogue/locals.tf
+++ 60-catalogue/locals.tf
@@ -3,7 +3,7 @@ locals {
   ami_id = data.aws_ami.joindevops.id
   catalogue_sg_id = data.aws_ssm_parameter.catalogue_sg_id
   vpc_id = data.aws_ssm_parameter.vpc_id
-  private_subnet_ids = split("," , data.aws_ssm_parameter.private_subnet_ids)
+  private_subnet_id = split("," , data.aws_ssm_parameter.private_subnet_ids.value)[0]
   common_tags = {
     Project = var.project_name
     Environment = var.environment
