983ddcd1fcbcd8b411023c717342719948e98ce1
Poguri Diya <diya>
2026-02-14 18:24:11 +0530

    terraform

--- 60-catalogue/main.tf
+++ 60-catalogue/main.tf
@@ -3,7 +3,7 @@
 resource "aws_instance" "catalogue" {
   ami                    = local.ami_id
   instance_type          = "t3.micro"
-  vpc_security_group_ids = [local.catalogue_sg_id.value]
+  vpc_security_group_ids = [local.catalogue_sg_id]
   subnet_id              = local.private_subnet_id
   tags = merge (
     local.common_tags,
