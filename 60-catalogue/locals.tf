200f9e7af0c8eab5453fe456293f52175728d6cd
Poguri Diya <diya>
2026-02-14 18:43:23 +0530

    terraform

--- 60-catalogue/locals.tf
+++ 60-catalogue/locals.tf
@@ -2,7 +2,7 @@ locals {
   common_name_suffix = "${var.project_name}-${var.environment}" # roboshop-dev
   ami_id = data.aws_ami.joindevops.id
   catalogue_sg_id = data.aws_ssm_parameter.catalogue_sg_id.value
-  vpc_id = data.aws_ssm_parameter.vpc_id
+  vpc_id = data.aws_ssm_parameter.vpc_id.value
   private_subnet_id = split("," , data.aws_ssm_parameter.private_subnet_ids.value)[0]
   private_subnet_ids = split(",", data.aws_ssm_parameter.private_subnet_ids.value)
   backend_alb_listener_arn = data.aws_ssm_parameter.backend_alb_listener_arn.value
