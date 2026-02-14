e9a41b2ea2b687fa623da74d19030a5a01364fe2
Poguri Diya <diya>
2026-02-14 15:52:00 +0530

    terraform

--- 40-databases/main.tf
+++ 40-databases/main.tf
@@ -122,7 +122,7 @@ resource "aws_instance" "mysql" {
 }
 resource "aws_iam_instance_profile" "mysql" {
   name = "mysql"
-  role = "EC2SSMParameterstore"
+  role = "EC2SSMParameterRead"
 }
 
 resource "terraform_data" "mysql" {
