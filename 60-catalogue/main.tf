93559382fa702ad9fc107b2910cfbdb8f5d45e11
Poguri Diya <diya>
2026-02-14 16:19:31 +0530

    terraform

--- 60-catalogue/main.tf
+++ 60-catalogue/main.tf
@@ -32,34 +32,62 @@ resource "terraform_data" "catalogue" {
   }
   provisioner "remote-exec" {
     inline = [
-      "chmod +x /tmp/bootstrap.sh" ,
-      "sudo sh /tmp/bootstrap.sh catalogue ${var.environment}"
+      "chmod +x /tmp/catalogue.sh" ,
+      "sudo sh /tmp/catalogue.sh catalogue ${var.environment}"
     ]
   }
 }
 
-# stop the instance to take ami image
-resource "aws_ec2_instance_state" "catalogue" {
-  instance_id = aws_instance.catalogue.id
-  state       = "stopped"
-  depends_on  = [terraform_data.catalogue]
-}
-resource "aws_ami_from_instance" "catalogue" {
-  name               = "${local.common_name_suffix}-catalogue-ami"
-  source_instance_id = aws_instance.catalogue.id
-  depends_on         = [ aws_ec2_instance_state.catalogue ]
-  tags                   = merge (
-    local.common_tags,
-    {
-        Name             = "${var.project_name}-${var.environment}-catalogue" #roboshop-dev-catalogue
-    }
-  )
-}
-# create target group
-resource "aws_lb_target_group" "catalogue"{
+# # stop the instance to take ami image
+# resource "aws_ec2_instance_state" "catalogue" {
+#   instance_id = aws_instance.catalogue.id
+#   state       = "stopped"
+#   depends_on  = [terraform_data.catalogue]
+# }
+# resource "aws_ami_from_instance" "catalogue" {
+#   name               = "${local.common_name_suffix}-catalogue-ami"
+#   source_instance_id = aws_instance.catalogue.id
+#   depends_on         = [ aws_ec2_instance_state.catalogue ]
+#   tags                   = merge (
+#     local.common_tags,
+#     {
+#         Name             = "${var.project_name}-${var.environment}-catalogue" #roboshop-dev-catalogue
+#     }
+#   )
+# }
+# # create target group
+# resource "aws_lb_target_group" "catalogue"{
+#   name = "${local.common_name_suffix}-catalogue"
+#   port = 8080
+#   protocol = "HTTP"
+#   vpc_id = local.vpc_id
+#   deregistration_delay = 60 # waiting period before deleting the instance
 
-}
-# create launch template
-# create auto scaling group
-# create auto scaling policy
-# create listener rule
+#   health_check {
+#     healthy_threshold = 2
+#     interval = 10
+#     matcher = "200-299"
+#     path = "/health"
+#     port = 8080
+#     protocol = "HTTP"
+#     timeout = 2
+#     unhealthy_threshold = 2
+#   }
+# }
+# # create launch template
+# resource "aws_launch_template" "catalogue"{
+#   name = "${local.common_name_suffix}-catalogue"
+#   image_id = aws_ami_from_instance.catalogue.id
+#   # instance_initiated_shutdown_behaviour = "terminate"
+#   instance_type = "t3.micro"
+
+#   vpc_security_group_ids = [local.catalogue_sg_id]
+
+#   # tags attached to the instance
+#   tags_specifications {
+#     resource_type
+#   }
+# }
+# # create auto scaling group
+# # create auto scaling policy
+# # create listener rule
