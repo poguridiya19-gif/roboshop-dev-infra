a5720274a446519d7fd80ff5f06f1fae01108cda
Poguri Diya <diya>
2026-02-16 13:48:54 +0530

    terraform

--- 30-sg-rules/main.tf
+++ 30-sg-rules/main.tf
@@ -71,3 +71,12 @@ resource "aws_security_group_rule" "catalogue_mongodb" {
   protocol                 = "tcp"
   to_port                  = 27017
 }
+
+resource "aws_security_group_rule" "catalogue_backend_alb" {
+  type              = "ingress"
+  security_group_id = local.catalogue_sg_id
+  source_security_group_id = local.backend_alb_sg_id
+  from_port         = 8080
+  protocol          = "tcp"
+  to_port           = 8080
+}
