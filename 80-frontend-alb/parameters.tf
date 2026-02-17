3b97adabfa743926375917140558614f399fe4e6
Poguri Diya <diya>
2026-02-17 13:31:57 +0530

    terraform

--- 80-frontend-alb/parameters.tf
+++ 80-frontend-alb/parameters.tf
@@ -1,5 +1,5 @@
 resource "aws_ssm_parameter" "frontend_alb_listener_arn" {
     name = "/${var.project_name}/${var.environment}/frontend_alb_listener_arn"
-    type = "string"
+    type = "String"
     value = aws_lb_listener.frontend_alb.arn
 }
