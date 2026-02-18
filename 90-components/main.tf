f608e353d23ca5e9ea221bedd90f96eba81231c7
Poguri Diya <diya>
2026-02-18 14:14:54 +0530

    terraform

--- 90-components/main.tf
+++ 90-components/main.tf
@@ -1,5 +1,5 @@
 module "components" {
-    source = "../../terraform-roboshop-component"
+    source = "git::https://github.com/poguridiya19-gif/terraform-roboshop-component.git"
     component = var.component
     rule_priority = var.rule_priority
 }
