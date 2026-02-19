8796ba3d228eec220f5b5ff88221b26584596229
Poguri Diya <diya>
2026-02-19 12:50:42 +0530

    terraform

--- 21-vpn/main.tf
+++ 21-vpn/main.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "openvpn" {
     instance_type = "t3.micro"
     vpc_security_group_ids = [local.openvpn_sg_id]
     subnet_id = local.public_subnet_id
-    user_data = file("vpn.sh")
+    # user_data = file("vpn.sh")
     
     tags = merge (
         local.common_tags,
